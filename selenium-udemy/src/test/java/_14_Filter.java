61525d1ec393f006035222c6f18d72e4aa605cb6
furkantatlisu <furkan58tatlisu@gmail.com>
2022-10-11 00:32:21 +0300

    scenario created

--- selenium-udemy/src/test/java/_14_Filter.java
+++ selenium-udemy/src/test/java/_14_Filter.java
@@ -1,6 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class _14_Filter {
 
@@ -10,6 +15,12 @@ public class _14_Filter {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
 
-        driver.findElement(By.xpath(""));
+        WebElement searchBoxElement = driver.findElement(By.id("search-field"));
+        searchBoxElement.sendKeys("Rice");
+        List<WebElement> listFoundElement = driver.findElements(By.xpath("//tr/td[1]"));
+        List<WebElement> filteredList = listFoundElement.stream().filter(s->s.getText().contains("Rice")).collect(Collectors.toList());
+        Assert.assertEquals(listFoundElement.size(),filteredList.size(),"is not equal");
+
+
     }
 }
